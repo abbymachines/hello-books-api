3d43f0c70a086744f6a8de51e7745c9fffee7cfc
Abby Castillo <abby.machines@gmail.com>
2023-04-25 15:36:21 -0700

    i made the book. and i made the books

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+class Book:
+    
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "mobys dick", "huge whale"),
+    Book(2, "the war and the peace", "so much happening"),
+    Book(3, "the bible", "the word of god"),
+    Book(4, "The Legend of Zelda: The Wind Waker Official Strategy Guide", "all the tips and tricks you need to win the game")
+]
+
